91baf3432ef419d4ed95610aac1190d4b7c4492a
CloudDevOpsHub <100401787+CloudDevOpsHub@users.noreply.github.com>
2025-12-09 20:06:57 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-sudo apt update -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Install JRE
+sudo apt install openjdk-17-jre-headless -y
+
+# Create keyrings folder
+sudo mkdir -p /usr/share/keyrings
+
+# Download Jenkins GPG key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+
+# Add Jenkins repo
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/" | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update
 sudo apt-get update -y
+
+# Install Jenkins
 sudo apt-get install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl status jenkins
